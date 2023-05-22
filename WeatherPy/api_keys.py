e25aefefea7928b00d778f0af121043f450e420e
Juslima <130790909+Juslima@users.noreply.github.com>
2023-05-21 18:48:00 -0700

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "103e589f1ee037fbbda88782d124f8ac"
+weather_api_key = "xxxxxxxxxxxxxxxxxxx"
 
 # Geoapify API Key
-geoapify_key = "f3a064fed6f8465e958cfd3ce863c901"
+geoapify_key = "xxxxxxxxxxxxxxxxxxxxxxx"
